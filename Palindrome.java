600da0fa61b74b583d0a68a8cb9aa14e3f385cd9
Nitha5599 <nithamurali3@gmail.com>
2024-09-12 19:16:17 +0530

    Upload a Palindrome using String

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+package StringExam;
+
+public class Palindrome {
+	public static void checkPalindrome(String str) {
+        String original = str.toLowerCase();
+        String rev = "";
+
+        for (int i = original.length()- 1; i>= 0; i--) {
+            rev += original.charAt(i);
+        }
+
+        if (original.equals(rev)) {
+            System.out.println(str + " is a Palindrome.");
+        } else {
+            System.out.println(str + " is not a Palindrome.");
+        }
+    }
+	public static void main(String[] args) {
+		checkPalindrome("Java");
+		checkPalindrome("Malayalam");
+	}
+}
